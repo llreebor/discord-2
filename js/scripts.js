aab2b75ed34eae3136961b664f029e7f7317a4fe
mac <mac@Roman-Timoshchuk.local>
2023-01-30 09:03:26 +0200

    base

---
+++ js/scripts.js
@@ -0,0 +1,105 @@
+// ************ Tabs ************
+function tabs(
+    headerSelector,
+    tabSelector,
+    contentSelector,
+    activeClass,
+    display = 'flex',
+) {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector)
+    function hideTabContent() {
+        content.forEach((item) => {
+            item.style.display = 'none'
+        })
+        tab.forEach((item) => {
+            item.classList.remove(activeClass)
+        })
+    }
+    function showTabContent(i = 0) {
+        content[i].style.display = display
+        tab[i].classList.add(activeClass)
+    }
+    hideTabContent()
+    showTabContent()
+    header.addEventListener('click', (e) => {
+        const target = e.target
+        if (
+            target.classList.contains(tabSelector.replace(/\./, '')) ||
+            target.parentNode.classList.contains(tabSelector.replace(/\./, ''))
+        ) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            })
+        }
+    })
+}
+if (document.querySelector('.tabs__header')) {
+    tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
+}
+
+// Select
+function toggleSelect() {
+    const select = document.querySelector('#select')
+    const selectDropdown = document.querySelector('.select__dropdown')
+    const selectBackdrop = document.querySelector('.select__backdrop')
+    select.addEventListener('click', () => {
+        selectDropdown.classList.toggle('open')
+        select.classList.toggle('open')
+    })
+
+    selectBackdrop.addEventListener('click', () => {
+        selectDropdown.classList.remove('open')
+    })
+}
+if (document.querySelector('#select')) {
+    toggleSelect()
+}
+// ************ Messages Dropdown toggle  ************
+function editMessage() {
+    const btns = document.querySelectorAll('.message__btn')
+    const dropdown = document.querySelectorAll('.message__dropdown')
+
+    btns.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            dropdown[i].classList.toggle('active')
+        })
+    })
+}
+editMessage()
+
+// ************ Light Mode ************
+function lightMode() {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    const checkbox = document.querySelector('.switch__checkbox')
+    const btn = document.querySelector('.switch__checkbox')
+
+    if (
+        localStorage.theme === 'light' ||
+        (!('theme' in localStorage) &&
+            window.matchMedia('(prefers-color-scheme: light)').matches)
+    ) {
+        document.body.classList.add('light')
+        checkbox.checked = true
+    } else {
+        document.body.classList.remove('light')
+        checkbox.checked = false
+    }
+
+    btn.addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('light')
+
+        if (document.querySelector('body').classList.contains('light')) {
+            localStorage.theme = 'light'
+            checkbox.checked = true
+        } else {
+            localStorage.theme = 'dark'
+            checkbox.checked = false
+        }
+    })
+}
+lightMode()
